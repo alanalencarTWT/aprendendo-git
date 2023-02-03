b44bf127262016d64ffc62c6eaf5baa091565688
Francieli Fujie <franfujie7@gmail.com>
2023-02-02 22:30:52 -0300

    primeiro programa calc fran

---
+++ funcao.js
@@ -0,0 +1,24 @@
+function soma() {
+    console.log ('A SOMA DE 5 + 2 =', 5+2);
+}
+
+soma();
+
+
+function subtracao(num1,num2) {
+    console.log ('a subtração de ',num1, '-',  num2, '=', num1 - num2);
+}
+    
+subtracao(-5,2);
+
+subtracao(5,7);
+
+subtracao(9,4);
+
+function doida (num1,num2,num3) {
+    console.log ('resposta doida', (num1 + num2) * num3);
+
+}
+
+doida(5,500,3); //11
+
