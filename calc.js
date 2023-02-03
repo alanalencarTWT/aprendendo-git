ff8f891133b86997d9adb8d5105add69735ff91a
Alan Alencar <alan.alencar@twtinfo.com.br>
2023-02-02 21:36:44 -0300

    Programa exemplo com js para calcular operações matemáticas básicas.

---
+++ calc.js
@@ -0,0 +1,34 @@
+// 1° forma
+console.log('A soma de 5 + 2 é igual =', 5 + 2);
+console.log('A subtração entre 5 - 2 é igual =', 5 - 2);
+console.log('A multiplicação de 5 * 2 é igual =', 5 * 2);
+console.log('A divisão de 5 / 2 é igual =', 5 / 2, '\n');
+
+// 2° forma
+let soma = 5 + 2;
+let subtracao = 5 - 2;
+let multiplicacao = 5 * 2;
+let divisao = 5 / 2;
+let nome = 'Francieli';
+let numero = 'A';
+let segundoNumero = 2;
+
+console.log('Operações Matemáticas com Js entre 5 e 2');
+console.log('------------------------------------------------------')
+console.log('soma=', soma, 'Subtração=', subtracao, 'Multiplicação=', multiplicacao, 'Divisão=', divisao, '\n');
+
+console.log('Tipo da variável soma é', typeof soma);
+console.log('Tipo da variável nome é', typeof nome);
+console.log('tipo da variável numero é', typeof numero, '\n');
+
+console.log('Soma de uma variável String com um Number é', numero + segundoNumero);
+console.log('Subtração de uma variável String com um Number é', numero - segundoNumero);
+// até aqui numero era uma variável do tipo String, a partir da próxima instrução será um number.
+numero = 5;
+console.log('Multiplicação de uma variável String com um Number é', numero * segundoNumero);
+console.log('Divisão de uma variável String com um Number é', numero / segundoNumero, '\n');
+
+// Alan
+// Francieli
+// Aprendendo JS e Git e Cypress.
+
